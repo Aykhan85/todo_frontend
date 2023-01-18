e03264e8de4bb299b063ed74370586340c128baf
OMEN <ayxannematov96@gmail.com>
2023-01-18 17:05:28 +0400

    todo app is ready to use

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://todo-app-api-zckg.onrender.com",
+  }),
   tagTypes: ["Todo"],
   endpoints: (builder) => ({}),
 });
